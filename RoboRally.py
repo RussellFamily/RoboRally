fdf8790bf3d961929840d1b9f4b36d10bea0bb19
Matt Russell <astromars42@gmail.com>
2015-01-13 16:56:00 -0600

    added RoboRally.py file

---
+++ RoboRally.py
@@ -0,0 +1,16 @@
+#Classes used in the game
+#Game object that starts each game and controls game flow
+class Game():
+	
+	def __init__(self):
+		
+		
+###########################		
+#Card object is used by robots to move or rotate, can either be in a deck, discard, hand, or register (locked or unlocked)
+###########################
+class Card(priority,type):
+	#initialize object
+	def __init__(self):
+		self.priority=priority
+		self.type=typerally.py
+###########################
