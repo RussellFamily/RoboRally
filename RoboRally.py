9e5408cfc2bf2be2e2609835f3d7d461c45b0664
PM8K <astromars42@gmail.com>
2015-06-01 16:53:26 -0500

    added game functionality

--- RoboRally.py
+++ RoboRally.py
@@ -1,14 +1,111 @@
 #Modules!
 import random
 import yaml
+import pygame
 #Classes used in the game
+###########################
+#Game Object that runs the display, handling updates to the screen
+###########################
+class Display():
+	def __init__(self):
+		pygame.init()
+		self.screensize=(1200,800)
+		self.screen=pygame.display.set_mode(self.screensize)
+		
+
+
+
+###########################
 #Game object that starts each game and controls game flow
 ###########################
+
 class Game():
 
 	def __init__(self):
 		self.playerlist=[]
 		self.deck=Deck()
+		
+
+	def run_game(self):
+		#get the board and players setup to play
+		self.game_setup()
+		#run game
+		self.play_game()
+		
+		
+	def assign_registers(self):
+		for player in self.playerlist:
+			if not player.robot.shutdown and not player.robot.dead:
+				while True:
+					#print hand and registers
+					player.print_hand()
+					player.print_registers()
+					
+					try:
+						card_choice=int(raw_input("Please choose a card you want to play based on the hand above: "))
+					except:
+						print 'The input was not a valid integer, please try again\n'
+						continue
+					if card_choice not in range(len(player.hand)):
+						print 'The integer was not a valid answer, please try again\n'
+						continue
+						
+					try:
+						reg_choice=int(raw_input("Which register do you want to put this card into?: "))
+					except:
+						print 'The input was not valid'
+						continue
+					if reg_choice not in range(1,6):
+						print 'not a valid register choice'
+						continue
+					elif player.robot.registers[reg_choice].card != None:
+						if player.robot.registers[reg_choice].register_lock==True:
+							print 'this register is locked, and cannot be changed'
+							continue
+						else:
+							player.hand.append(player.robot.registers[reg_choice].card)
+					
+					player.robot.registers[reg_choice].card=player.hand[card_choice]
+					#is it pop or drop?
+					player.hand.pop(card_choice)
+					
+					filled_regs=0
+					for reg in player.robot.registers:
+						if player.robot.registers[reg].card!=None:
+							filled_regs+=1
+					answer=''
+					if filled_regs==5:
+						player.print_hand()
+						player.print_registers()
+						while True:
+							answer=raw_input('Is this your final choice for registers (y/n)?')
+							if answer=='y':
+								break
+							if answer=='n':
+								break
+							else:
+								print 'not a valid response, enter again'
+
+					if answer=='y':
+						break
+		
+		
+	def play_game(self):
+		
+		#iterate through rounds of play
+		
+		#first step is to deal out cards to all players
+		self.deal_hands()
+		
+		#next, each player has to choose which order of cards they will be dealing out
+		#for the first iteration, each player will take turns individually choosing all of his/her cards through the terminal
+		#they will be told what cards they have in hand, which registers currently have cards chosen in them, and which ones are locked
+		#finally they will confirm if the choices are what they will make them out to be
+
+		self.assign_registers()
+		
+		#time to execute all of the moves register by register
+
 
 	def game_setup(self):
 		#Determine how many players are going to be playing the game
@@ -16,30 +113,55 @@ class Game():
 		print 'Welcome to Robo Rally!\n'
 		while True:
 			try:
-				x=int(raw_input('How many players are playing? Valid numbers are 2, 3, and 4.\n'))
+				num_players=int(raw_input('How many players are playing? Valid numbers are 2, 3, and 4.\n'))
 			except:
 				print 'Not a valid value. Please try again.'
 				continue
 			else:
-				if x in num_allowed_players:
+				if num_players in num_allowed_players:
 					break
 				else:
 					print 'Not a valid number of players. Please try again.'
 		
+		#READ in the names for each player
+		#maybe functionalize to reduce code here
+		playernames=[]
+		for i in range(num_players):
+			while True:
+				playername=raw_input('What is the name of Player '+str(i)+'?')
+				if len(playername)==0:
+					print 'Not a valid name. The length of the name must be at least 1 character'
+				elif len(playername)>10:
+					print 'Not a valid name. The length of the name is too long.'
+				elif playername in playernames:
+					print 'Not a valid name.  That name has already been taken. Please choose another name'
+				else:
+					break
+			playernames.append(playername)
 		#Determine board to be used - for now will be a test board - and load the dictionary of the board
-		self.board='test'
-		board_file=open(self.board+'/'+self.board+'_config.txt','r')
-		board_dict=yaml.load(board_file)
+		#self.board='test'
+		#board_file=open(self.board+'/'+self.board+'_config.txt','r')
+		#board_dict=yaml.load(board_file)
 		
 		#Convert board dict to usable input
+		#TODO
+		
+		#Assign robots to players - default arrangement for now
+		#TODO - make dynamic
+		robots=['r1','r2','r3','r4']
+		#list comprehension to create list of players
+		self.playerlist=[Player(player,robots[i]) for i,player in enumerate(playernames)]
+		#The game now has a list of the players, their names, and which robots they are playing!
+		#Setup is complete, time to play the game
+		
 		
 	#Function that deals out cards to all players
 	def deal_hands(self):
-		for player in playerlist:
+		for player in self.playerlist:
 			if player.robot.shutdown==True:
 				pass
 			else:
-				for i in range(player.robot.damage):
+				for i in range(10-player.robot.damage):
 					self.deal_card(player)
 
 	#Function used to deal out a single card to a player.  Used to check if deck is also empty when dealing
@@ -50,7 +172,7 @@ class Game():
 
 	#Execute move actions of all robots by sorting moves of active players
 	def execute_move_phase(register):
-		moveorder=[player for player in self.playerlist if player.robot.dead==False and player.robot.shutdown=False]
+		moveorder=[player for player in self.playerlist if player.robot.dead==False and player.robot.shutdown==False]
 		moveorder=sorted(moveorder,key=lambda player:player.robot.registers[register].priority)
 		for player in moveorder:
 			execute_move(player.robot.registers[register].cardtype)
@@ -59,6 +181,7 @@ class Game():
 	#def execute_move(movetype):
 
 ###########################
+
 #Card object is used by robots to move or rotate, can either be in a deck, discard, hand, or register (locked or unlocked)
 ###########################
 class Card():
@@ -116,10 +239,29 @@ class Deck():
 ###########################
 class Player():
 
-	def __init__(self,name,robot_name):
+	def __init__(self,name,robot_name='None'):
 		self.name=name
 		self.robot=Robot(robot_name)
 		self.hand=[]
+	
+	#prints the current hand of the player	
+	def print_hand(self):
+		print 'PLAYER HAND\n'
+		for i,card in enumerate(self.hand):
+			print str(i)+":"+card.cardtype+ " : " + str(card.priority)
+			
+	#prints out current registers of the player's robot
+	def print_registers(self):
+		print 'CURRENT REGISTERS\n'
+		for i in range(1,6):
+			register=self.robot.registers[i]
+			if register.card==None:
+				print str(i) + " : NONE"
+			elif register.register_lock==True:
+				print str(i)+" : " + register.card.cardtype + " : " + str(register.card.priority) + " LOCKED"
+			else:
+				print str(i)+" : " + register.card.cardtype + " : " + str(register.card.priority)
+			
 ###########################
 #Robot Object is created under a player.  It controls its registers, spot on the board, facing, and damage.
 ###########################
@@ -133,9 +275,12 @@ class Robot():
 		self.shutdown=False
 		self.dead=False
 		self.num_death=0
+		#positional and directional information for each bot
+		self.position=(0,0)
+		self.direction='n'
 
 
-	def initiate_registers():
+	def initiate_registers(self):
 		reg={}
 		for i in range(1,6):
 			reg[i]=Register(i)
