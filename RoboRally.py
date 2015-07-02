c67224d5e7580045192d69a498d069338e18d7a3
Matt Russell <astromars42@gmail.com>
2015-07-01 22:56:55 -0500

    updated image locations

--- RoboRally.py
+++ RoboRally.py
@@ -147,7 +147,7 @@ class Game():
 	def cleanup(self):
 		#determine any damage removal from checkpoints
 		for player in self.playerlist:
-			if self.board.board_dict[tuple(player.robot.postition)].checkpoint==True:
+			if self.board.board_dict[tuple(player.robot.position)].checkpoint==True:
 				if player.robot.damage>0:
 					player.robot.heal_damage(1)
 
@@ -888,7 +888,7 @@ class Robot():
 		self.next_flag=1
 
 	def load_image(self):
-		image=pygame.transform.scale(pygame.image.load('Images_v2/Robots/'+self.robot_name+'.jpg'),(100,100))
+		image=pygame.transform.scale(pygame.image.load('Images_v2/Robots/'+self.robot_name+'.png'),(100,100))
 		return image
 
 	def initiate_registers(self):
@@ -960,10 +960,10 @@ class Board():
 
 	#currently load all board elements into
 	def load_images(self):
-		board_spaces=['blank','checkpoint_1','checkpoint_2','fast','flag','laser_origin','laser','pit','straight','rotate_left','rotate_right','wall']
+		board_spaces=['blank','checkpoint_1','checkpoint_2','laser_origin','laser','pit','straight','rotate_left','rotate_right','wall']
 		image_dict={}
 		for space in board_spaces:
-			image_dict[space]=pygame.transform.scale(pygame.image.load('Images/Board_Elements/'+space+'.jpg'),(100,100))
+			image_dict[space]=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/'+space+'.png'),(100,100))
 		return image_dict
 
 	#have the board control rotations of the wall image when blitting the screen
@@ -1027,7 +1027,7 @@ class Boardspace():
 
 	def load_board_tile(self):
 		if self.boardtile=='cb':
-			pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/'++'.jpg'),(100,100))
+			pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/'++'.png'),(100,100))
 
 	#notes for feature coding
 	#all features will be coded into the features function as a dictionary
@@ -1046,22 +1046,22 @@ class Boardspace():
 
 		if self.boardtile=='cb':
 			self.cb=[True,Conveyor_Belt(boardtile_dict['cb'])]
-			self.boardtile_image=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/'+boardtile_dict['cb']['conveyor_type']+'.jpg'),(100,100))
+			self.boardtile_image=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/'+boardtile_dict['cb']['conveyor_type']+'.png'),(100,100))
 			self.rotate_image(boardtile_dict['cb']['orientation'])
 
 		elif self.boardtile=='blank':
-			self.boardtile_image=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/blank.jpg'),(100,100))
+			self.boardtile_image=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/blank.png'),(100,100))
 
 		elif self.boardtile=='pit':
-			self.boardtile_image=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/pit.jpg'),(100,100))
+			self.boardtile_image=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/pit.png'),(100,100))
 			self.pit=True
 
 		elif self.boardtile=='checkpoint_1':
-			self.boardtile_image=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/checkpoint_1.jpg'),(100,100))
+			self.boardtile_image=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/checkpoint_1.png'),(100,100))
 			self.checkpoint=True
 
 		elif self.boardtile=='checkpoint_2':
-			self.boardtile_image=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/checkpoint_2.jpg'),(100,100))
+			self.boardtile_image=pygame.transform.scale(pygame.image.load('Images_v2/Board_Elements/checkpoint_2.png'),(100,100))
 			self.checkpoint=True
 
 		#NEXT, we will see if there are any walls or lasers
